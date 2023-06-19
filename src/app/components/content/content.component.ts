9ea2f5fdddd00356a23216ed3de34f0fd4de4048
JhaddynOrtiz <jhaddyn.ortiz@salamancasolutions.com>
2023-06-18 20:14:45 -0400

    finish only fixed options

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import {
   trigger,
   state,
@@ -7,37 +7,62 @@ import {
   transition
 } from '@angular/animations';
 
+const transitionBanner = trigger('bannerState', [
+  state('state1', style({
+    transform: 'translateX(-100%)',
+    opacity: 0
+  })),
+  state('state2', style({
+    transform: 'translateX(0)',
+    opacity: 1
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const transitionPhone = trigger('phoneState', [
+  state('state1', style({
+    transform: 'translateY(20%)',
+    opacity: 0
+  })),
+  state('state2', style({
+    transform: 'translateY(0)',
+    opacity: 1
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
-  animations: [
-    trigger('enterState', [
-      state('void', style({
-        transform: 'translateX(-100%)',
-        opacity: 0
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateX(0)',
-          opacity: 1
-        }))
-      ])
-    ]),
-    trigger('phoneState', [
-      state('void', style({
-        transform: 'translateY(100%)',
-        opacity: .5
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)',
-          opacity: 1
-        }))
-      ])
-    ])
-  ]
+  animations: [ transitionBanner, transitionPhone ]
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  runBanner: string = 'state1';
+
+  @HostListener('window:scroll', ['$event'])
+  divScroll(e: any) {
+    const window = e.currentTarget as Window;
+    let currentPosition = window.scrollY;
+    if (currentPosition < 400) {
+      this.runBanner = 'state2';
+    } else if (currentPosition >= 400) {
+      this.runBanner = 'state1';
+    }
+  }
+
+  onStartBanner() {
+    this.runBanner = 'state2';
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.onStartBanner();
+    }, 200);
+  }
 
 }
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import {
   trigger,
   state,
@@ -7,83 +7,119 @@ import {
   transition
 } from '@angular/animations';
 
+const transitionTopPhone = trigger('topPhone', [
+  state('state1', style({
+    transform: 'translate(100%, -100%)'
+  })),
+  state('state2', style({
+    transform: 'translate-(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const trasitionMidPhone = trigger('midlePhone', [
+  state('state1', style({
+    transform: 'translate(-100%, 100%)'
+  })),
+  state('state2', style({
+    transform: 'translateY(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const trasitionBottomPhone = trigger('bottomPhone', [
+  state('state1', style({
+    transform: 'translate(100%, 100%)'
+  })),
+  state('state2', style({
+    transform: 'translateY(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const trasitionTitle = trigger('title-large', [
+  state('state1', style({
+    transform: 'translateY(-150%)'
+  })),
+  state('state2', style({
+    transform: 'translateY(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const trasitionSubTitle = trigger('subtitle-short', [
+  state('state1', style({
+    transform: 'translateY(-150%)'
+  })),
+  state('state2', style({
+    transform: 'translateY(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const trasitionImageGP = trigger('image-googlePlay', [
+  state('state1', style({
+    transform: 'translateY(150%)'
+  })),
+  state('state2', style({
+    transform: 'translateY(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
+const trasitionAvailable = trigger('availability-large', [
+  state('state1', style({
+    transform: 'translateY(800%)'
+  })),
+  state('state2', style({
+    transform: 'translateY(0)'
+  })),
+  transition('state1 <=> state2',
+    animate(800)
+  )
+]);
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
   animations: [
-    trigger('topPhone', [
-      state('void', style({
-        transform: 'translate(100%, -100%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translate-(0)'
-        }))
-      ])
-    ]),
-    trigger('midlePhone', [
-      state('void', style({
-        transform: 'translate(-100%, 100%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
-    trigger('bottomPhone', [
-      state('void', style({
-        transform: 'translate(100%, 100%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
-    trigger('title-large', [
-      state('void', style({
-        transform: 'translateY(-150%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
-    trigger('subtitle-short', [
-      state('void', style({
-        transform: 'translateY(-150%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
-    trigger('image-googlePlay', [
-      state('void', style({
-        transform: 'translateY(150%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
-    trigger('availability-large', [
-      state('void', style({
-        transform: 'translateY(800%)'
-      })),
-      transition(':enter', [
-        animate(800, style({
-          transform: 'translateY(0)'
-        }))
-      ])
-    ]),
+    transitionTopPhone, 
+    trasitionMidPhone, 
+    trasitionBottomPhone, 
+    trasitionTitle, 
+    trasitionSubTitle, 
+    trasitionImageGP, 
+    trasitionAvailable
   ]
 })
+
 export class FooterComponent {
+
+  runState: string = 'state1';
+
+  @HostListener('window:scroll', ['$event'])
+  divScroll(e: any) {
+    const window = e.currentTarget as Window;
+    let currentPosition = window.scrollY;
+    if (currentPosition > 4000) {
+      console.log('true');
+      this.runState = 'state2';
+    } else if (currentPosition <= 4000) {
+      this.runState = 'state1';
+    }
+  }
 
 }
